e52f62a4f8e99c7218fe23b52d48ae03229e4319
simketejong <simketejong@gmail.com>
2015-10-24 18:49:46 +0000

    Final touches PWM0

---
+++ PWM0/Run_PWM_0.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import commands
+import os
+from time import sleep
+from subprocess import PIPE, Popen
+
+def cmdline(command):
+    process = Popen(
+        args=command,
+        stdout=PIPE,
+        shell=True
+    )
+    return process.communicate()[0]
+
+test=commands.getstatusoutput('/bin/ps -ef | /bin/grep pwm0.py | /usr/bin/wc -l ')
+print test[1]
+if test[1] == "2":
+	os.system('/usr/bin/python /home/pi/ACD/ABElectronics_Python_Libraries/ADCPi/PWM0/pwm0.py &') 
+
+test=commands.getstatusoutput('/bin/ps -ef | /bin/grep read_pwm0.py | /usr/bin/wc -l')
+print test[1]
+if test[1] == "2":
+	os.system('/usr/bin/python /home/pi/ACD/ABElectronics_Python_Libraries/ADCPi/PWM0/read_pwm0.py &') 
+
+test=commands.getstatusoutput('/bin/ps -ef | /bin/grep PID_pwm0.py | /usr/bin/wc -l')
+print test[1]
+if test[1] == "2":
+	os.system('/usr/bin/python /home/pi/ACD/ABElectronics_Python_Libraries/ADCPi/PWM0/PID_pwm0.py &') 
+
+test=commands.getstatusoutput('/bin/ps -ef | /bin/grep Run_PWM_0.py | /usr/bin/wc -l')
+print test[1]
+if test[1] == "3":
+	while True:
+		laatste=cmdline('/usr/bin/tail -n 1 /tmp/rpm0.log')
+		sleep(5)
+		aller_laatste=cmdline('/usr/bin/tail -n 1 /tmp/rpm0.log')
+		if laatste == aller_laatste:
+			if laatste != "0":
+				fh = open("/tmp/rpm0.log", "a")
+				for x in range (0,20):
+					fh.write("0\n")
+				fh.close
+		#print("laatste=%s aller_laatste=%s"%(laatste,aller_laatste))
+				os.system('./size.sh')
+		sleep(5)
+
+
--- PWM0/size.sh
+++ PWM0/size.sh
@@ -1,10 +1,9 @@
  #!/bin/bash          
-
 file="/tmp/temperature.log"
 if [ -f "$file" ]
 then
- bestand=`ls -la /tmp | grep temperature | cut -d " " -f 10`
- if [ $bestand -gt 250000 ]
+ bestand=`/usr/bin.du -k /tmp/temperature.log | /usr/bin/cut -f1`
+ if [ $bestand -gt 250 ]
  	then
  		/usr/bin/tail -c 100KB /tmp/temperature.log > /tmp//temp 2>&1
 		/bin/mv /tmp/temp /tmp/temperature.log
@@ -15,8 +14,8 @@ fi
 file="/tmp/rpm0.log"
 if [ -f "$file" ]
 then
- bestand=`ls -la /tmp | grep rpm0 | cut -d " " -f 18`
- if [ $bestand -gt 250000 ]
+ bestand=`/usr/bin/du -k /tmp/rpm0.log | /usr/bin/cut -f1`
+ if [ $bestand -gt 250 ]
  	then
  		/usr/bin/tail -c 100KB /tmp/rpm0.log > /tmp//temp 2>&1
 		/bin/mv /tmp/temp /tmp/rpm0.log
